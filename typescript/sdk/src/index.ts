563064418f81f2d984e63a0e88e237c5e1bf44fe
ljankovic-txfusion <lazar@txfusion.io>
2025-05-06 12:55:36 +0200

    chore: merge feat/starknet-sdk-integration changes into feat/starknet-send-relayer

--- typescript/sdk/src/index.ts
+++ typescript/sdk/src/index.ts
@@ -61,7 +61,6 @@ export { CosmWasmCoreAdapter } from './core/adapters/CosmWasmCoreAdapter.js';
 export { EvmCoreAdapter } from './core/adapters/EvmCoreAdapter.js';
 export { SealevelCoreAdapter } from './core/adapters/SealevelCoreAdapter.js';
 export { StarknetCoreAdapter } from './core/adapters/StarknetCoreAdapter.js';
-
 export { ICoreAdapter } from './core/adapters/types.js';
 export {
   CoreAddresses,
